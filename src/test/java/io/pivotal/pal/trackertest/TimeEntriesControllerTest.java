23135e4e5098ee351ae6e1b69d319b136469f311
sanjayhcl <sanjay.saxena@hcl.com>
2017-07-19 17:10:19 -0600

    ok

--- src/test/java/io/pivotal/pal/trackertest/TimeEntriesControllerTest.java
+++ src/test/java/io/pivotal/pal/trackertest/TimeEntriesControllerTest.java
@@ -8,6 +8,8 @@ import org.junit.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import java.util.List;
+import org.springframework.boot.actuate.metrics.CounterService;
+import org.springframework.boot.actuate.metrics.GaugeService;
 
 import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
